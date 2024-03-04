de17fdbc710f2a3e30d21fd15f2e15658173e4c6
ttrlf <qmdlxlr@gmail.com>
2024-03-04 23:18:33 +0900

    init

---
+++ app/list/page.js
@@ -0,0 +1,17 @@
+export default function List() {
+    let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+    return (
+      <div> 
+        <h4 className="title">상품목록</h4>
+        { 
+          상품.map((a, i)=>{
+            return ( 
+              <div className="food" key={i}> 
+                <img src={`/food${i}.png`} className="food-img"/>
+                <h4>{ a } $40</h4>
+              </div>
+            )
+        }) }
+      </div>
+    )
+  }
