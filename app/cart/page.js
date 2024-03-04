6fd8793a9107be58babc730e264ffa22e561b505
ttrlf <qmdlxlr@gmail.com>
2024-03-05 00:04:37 +0900

    useState, onClick

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,17 +1,27 @@
 export default function Cart() {
+  let 장바구니 = ['Tomatoes', 'Pasta']
+
     return (
       <div>
         <h4 className="title">Cart</h4>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
+        <CartItem item={장바구니[0]}/>
+        <CartItem item={장바구니[1]}/>
+        <Banner content="롯데카드"/>
+        <Banner content="현대카드"/>
       </div>
     )
   } 
+  
+  function Banner(props){
+    return <h5>{props.content} 결제 행사 중</h5>
+  }
+
+  function CartItem(props){
+    return (
+      <div className="cart-item">
+      <p>{props.item}</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>
+    )
+  }
--- app/list/page.js
+++ app/list/page.js
@@ -1,17 +1,25 @@
+'use client'
+
+import { useState } from "react"
+
 export default function List() {
-    let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
-    return (
-      <div> 
-        <h4 className="title">상품목록</h4>
-        { 
-          상품.map((a, i)=>{
-            return ( 
-              <div className="food" key={i}> 
-                <img src={`/food${i}.png`} className="food-img"/>
-                <h4>{ a } $40</h4>
-              </div>
-            )
-        }) }
-      </div>
-    )
-  }
+  let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+  let [수량, 수량변경] = useState(0)
+  return (
+    <div>
+      <h4 className="title">상품목록</h4>
+      {
+        상품.map((a, i) => {
+          return (
+            <div className="food" key={i}>
+              <img src={`/food${i}.png`} className="food-img" />
+              <h4>{a} $40</h4>
+              <span> {수량} </span>
+              <button onClick={() => { 수량변경(수량 + 1) }}>+</button>
+              <button onClick={() => { 수량변경(수량 - 1) }}>-</button>
+            </div>
+          )
+        })}
+    </div>
+  )
+}
