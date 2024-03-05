0e4933421aa78303db4a6b3ae4c5535bec72aa5a
ttrlf <qmdlxlr@gmail.com>
2024-03-05 20:19:43 +0900

    array,object state

--- app/list/page.js
+++ app/list/page.js
@@ -4,7 +4,7 @@ import { useState } from "react"
 
 export default function List() {
   let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
-  let [수량, 수량변경] = useState(0)
+  let [수량, 수량변경] = useState([0, 0, 0])
   return (
     <div>
       <h4 className="title">상품목록</h4>
@@ -14,9 +14,19 @@ export default function List() {
             <div className="food" key={i}>
               <img src={`/food${i}.png`} className="food-img" />
               <h4>{a} $40</h4>
-              <span> {수량} </span>
-              <button onClick={() => { 수량변경(수량 + 1) }}>+</button>
-              <button onClick={() => { 수량변경(수량 - 1) }}>-</button>
+              <span> {수량[i]} </span>
+              <button onClick={() => {
+                let copy = [...수량]
+                copy[i]++
+                수량변경(copy)
+              }}>+</button>
+
+              <button onClick={() => {
+                let copy = [...수량]
+                copy[i]--
+                수량변경(copy)
+              }}>-</button>
+
             </div>
           )
         })}
